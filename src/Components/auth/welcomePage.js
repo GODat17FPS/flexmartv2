9080f33b7585aab9cb3d356c2ef91f6bfe7f6cd2
Anubhav <anubhavsarkar44@gmail.com>
2020-05-18 16:46:38 +0530

    new-commit 2

--- src/Components/auth/welcomePage.js
+++ src/Components/auth/welcomePage.js
@@ -3,17 +3,28 @@ import { Link } from "react-router-dom";
 import "./welcomePage.css";
 function welcomePage() {
   return (
-    <div className="welcome-page">
-      <div className="welcome-head">
-        <h3>Welcome to FlexMart</h3>
+    <div class="x">
+      <div class="bg">
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
       </div>
-      <div className="welcome-components">
-        <Link className="welcome-x" to="/SignUp/Customer">
-        <button type="button" className="btn btn-dark">Sign Up</button>
-        </Link>
-        <Link className="welcome-x" to="/Login">
-        <button type="button" className="btn btn-dark">Login</button>
-        </Link>
+
+      <div className="welcome-page">
+        <div className="welcome-head">
+          <h3>Welcome to FlexMart</h3>
+        </div>
+        <div className="welcome-components">
+          <Link className="welcome-x" to="/SignUp/Customer">
+            <button type="button" className="btn btn-dark">
+              Sign Up
+            </button>
+          </Link>
+          <Link className="welcome-x" to="/Login">
+            <button type="button" className="btn btn-dark">
+              Login
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
